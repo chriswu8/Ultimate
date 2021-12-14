6a2f0f1e16204d306612f6ba08b9717005c8f604
Chris Wu <solus@Chriss-MacBook-Pro.local>
2021-12-13 23:35:42 -0800

    delete index.html, modified login.html, main.html, and scripts/firebaseAPI.js

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,13 +1,12 @@
 // Your web app's Firebase configuration
 var  firebaseConfig = {
-    apiKey: ,
-    authDomain: ",
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId: 
+  apiKey: "AIzaSyCSqV9gOHaygR-ukK1pPvnGlNwxeZIveuo",
+  authDomain: "ulti-app-a28ad.firebaseapp.com",
+  projectId: "ulti-app-a28ad",
+  storageBucket: "ulti-app-a28ad.appspot.com",
+  messagingSenderId: "29734538082",
+  appId: "1:29734538082:web:ef1048bf1e0abdd5d47a3b"
   };
-  
 
   const app = firebase.initializeApp(firebaseConfig);
-  
+  const db = firebase.firestore();
